b812bb4df929e3f30f5655641798002bad3da5f6
Isaac Boda <isaac@boxjaw.com>
2017-08-28 11:04:53 -0400

    Posts are being fetched and parsed now

--- scripts/fetchPosts.js
+++ scripts/fetchPosts.js
@@ -1,10 +1,22 @@
 var mongoose = require('mongoose');
 var _ = require('lodash');
+var rp = require('request-promise');
 
 function fetchPosts(callback) {
   setTimeout(function(){
     console.log('Running fetchPosts Process.');
-  }, 20000);
+    rp('http://www.reddit.com/r/politics/rising/.json')
+    .then(function(htmlString) {
+      var jsonData = JSON.parse(htmlString);
+      var importantData = jsonData.data.children;
+        importantData.forEach(function(value){
+          console.log(value.data)
+        })
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+  }, 120000);
 }
 
 //Run this script every 10 seconds
