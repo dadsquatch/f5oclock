81efaa8c93852e8df676cf68b9109c0c59d28aa2
Isaac Boda <isaac@boxjaw.com>
2017-08-29 23:57:23 -0400

    changing port back for nginx port forward

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get('/getPosts', function(req, res){
   })
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3030, function () {
+  console.log('Example app listening on port 3030!')
 })
