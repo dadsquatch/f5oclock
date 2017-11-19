f9b422f084d17c22df9c181ce3dda5973f6dccdc
dantheuber <dan.essig@gmail.com>
2017-11-19 13:59:52 -0700

    fix gitignore

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  mongo: {
+    uri: 'mongodb://localhost/f5oclock'
+  }
+};
---
+++ config/production.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  mongo: {
+    uri: "mongodb://f5oclock:$5]S2Jj-PxE@$M(_@ds113566.mlab.com:13566/heroku_w5x4rzvr"
+  }
+};
