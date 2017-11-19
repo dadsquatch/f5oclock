db0c8fb0a418e5326b70b8ada58b0a4a6c669d10
dantheuber <dan.essig@gmail.com>
2017-11-19 14:18:31 -0700

    use env pass

--- config/production.js
+++ config/production.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   mongo: {
-    uri: "mongodb://f5oclock:f50clockDBuser123!@ds113566.mlab.com:13566/heroku_w5x4rzvr"
+    uri: "mongodb://f5oclock:" + process.env.MONGO_PASSWORD + "@ds113566.mlab.com:13566/heroku_w5x4rzvr"
   }
 };
